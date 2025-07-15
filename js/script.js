d4b70db01c5e04ab70bd8d0b7dc637cfe6309b44
dmitriyShatalin19 <shatal1d@yandex.ru>
2025-07-15 18:05:51 +0300

    пост меняется на сайти

--- js/script.js
+++ js/script.js
@@ -1,10 +1,11 @@
 let postTitle = '';
 
 const titleInputNode = document.querySelector('.js-title-input');
-const newPostBtnNode = document.querySelector('.js-new-post-btn')
+const newPostBtnNode = document.querySelector('.js-new-post-btn');
+const postsNode = document.querySelector('.js-posts');
 
 newPostBtnNode.addEventListener('click', function() {
-    postTitle = titleInputNode.value
+    postTitle = titleInputNode.value;
 
-    console.log(postTitle);
-})
+    postsNode.innerText = postTitle;
+});
