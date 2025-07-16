cae629d3a748c78df1039be7d844bc0f406b93d7
dmitriyShatalin19 <shatal1d@yandex.ru>
2025-07-16 16:32:07 +0300

    сделал ограничение поста

--- js/script.js
+++ js/script.js
@@ -1,24 +1,49 @@
 const posts = [];
 
+const TITLE_VALIDATION_LIMIT = 15;
+const TEXT_VALIDATION_LIMIT = 40;
+
 const postTitleInputNode = document.querySelector('.js-post-title-input');
 const postTextInputNode = document.querySelector('.js-post-text-input');
 const newPostBtnNode = document.querySelector('.js-new-post-btn');
 const postsNode = document.querySelector('.js-posts');
+const validatonMassage = document.querySelector('.validationMessage');
 
-newPostBtnNode.addEventListener('click', function() {
+
+newPostBtnNode.addEventListener('click', function() { // обработка при клики
     const postFromUser = getPostFromUser();
     addPost(postFromUser);
     renderPosts();
 });
+postTitleInputNode.addEventListener('input',validation);
+postTextInputNode.addEventListener('input',validation);
 
+function validation() { // ограничение поста
+    const titleLen = postTitleInputNode.value.length;
+    const textLen = postTextInputNode.value.length;
+
+    if (titleLen > TITLE_VALIDATION_LIMIT) {
+        validatonMassage.innerText = `Длина заголовка не должна превышать ${TITLE_VALIDATION_LIMIT} символов`;
+        validatonMassage.classList.remove('validationMessege_hidden');
+        return;
+    }
+
+    if (textLen > TEXT_VALIDATION_LIMIT) {
+        validatonMassage.innerText = `Длина текса не должна превышать ${TEXT_VALIDATION_LIMIT} символов`;
+        validatonMassage.classList.remove('validationMessege_hidden');
+        return;
+    }
+
+    validatonMassage.classList.add(`validationMessege_hidden`);
+}
 function getPostFromUser() { // возвращает пост
     const title = postTitleInputNode.value;
     const text = postTextInputNode.value;
-
+    
     return {
         title: title,
         text: text,
-    };
+    }
 }
 function addPost({title, text}) { // сохраняет пост
     posts.push({
